157198aa0c31139e6872e8d99ff68aa1a5f180f2
Hugo Estrada <hugoestr@gmail.com>
2020-02-29 09:37:47 -0500

    Removing a button that does nothing. Changing config to allow bigger upload limits

--- sales_admin/lib/sales_admin_web/endpoint.ex
+++ sales_admin/lib/sales_admin_web/endpoint.ex
@@ -29,6 +29,7 @@ defmodule SalesAdminWeb.Endpoint do
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
+    length: 50_000_000, 
     json_decoder: Phoenix.json_library()
 
   plug Plug.MethodOverride
